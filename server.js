0b89ffa05d0a31161f53eee2c4dec09be8e82b4f
Jayant Rai <jayant.chamling@outlook.com>
2019-11-14 23:39:56 -0800

    added server.js file

---
+++ server.js
@@ -0,0 +1,37 @@
+// to host the backend server
+const express = require("express");
+// to provide a restful web service
+const jsonServer = require("json-server");
+// to moniter files for changes
+const chokidar = require("chokidar");
+// to enable cross origin request sharing
+const cors = require("cors");
+
+const fileName = process.argv[2] || "./data.js"
+const port = process.argv[3] || 3500;
+
+let router = undefined;
+
+const app = express();
+
+const createServer = () => {
+    delete require.cache[require.resolve(fileName)];
+    setTimeout(() => {
+        router = jsonServer.router(fileName.endsWith(".js") 
+            ? require(fileName)() : fileName);
+    }, 100)
+}
+
+createServer();
+
+app.use(cors());
+app.use(jsonServer.bodyParser)
+app.use("/api", (req, resp, next) => router(req, resp, next));
+
+chokidar.watch(fileName).on("change", () => {
+    console.log("Reloading web service data...");
+    createServer();
+    console.log("Reloading web service data complete.");
+});
+
+app.listen(port, () => console.log(`Web service running on port ${port}`));
